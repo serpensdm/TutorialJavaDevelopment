77836773c49f28b167299340086068d7d31d58c6
serpensdm <91413499+serpensdm@users.noreply.github.com>
2022-06-21 20:52:49 +0300

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    short a = 12;
+
     byte b = 123;
     int i = -123;
     String s = "хуй пизда и джигуржда";
-    System.out.println(a+b+i+s);
-    //фывфыв
+    System.out.println(b+i+s);
+
 
 
     }
