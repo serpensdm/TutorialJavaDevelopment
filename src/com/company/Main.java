676f5fff52bd108884dd5f561a03e7252b7ba9c3
serpensdm <91413499+serpensdm@users.noreply.github.com>
2022-06-21 20:34:23 +0300

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 10;
-        System.out.println(x);
-        int y = 20;
-        int yasd = 123;
-        short ячс = 123;
+    //123
     }
 }
